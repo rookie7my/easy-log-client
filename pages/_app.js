c83a1a21736f8b799fe371cf2e358795225b688a
jm kim <rookie2000g@gmail.com>
2022-01-27 17:16:28 +0900

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import 'minireset.css/minireset.css';
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
