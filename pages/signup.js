215ff9e92785f3798a7ca382ed8e3d99df81248e
jm kim <rookie2000g@gmail.com>
2022-01-28 16:37:21 +0900

    Add signup.js and SignUpForm component

---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+import SignUpForm from "../components/SignUpForm/SignUpForm";
+
+const SignUp = () => {
+  return (
+    <>
+      <NavBar />
+      <SignUpForm />
+    </>
+  );
+};
+
+export default SignUp;
