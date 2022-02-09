6c90a13f903a90fc9fb4070765e474821aaca061
jm kim <rookie2000g@gmail.com>
2022-02-09 14:56:58 +0900

    Feat(Header): Add links

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,21 @@
-import Image from "next/image";
-import styles from "./styles.module.css";
+import Image from 'next/image';
+import Link from 'next/link'
+import styles from './styles.module.css';
 
 const Header = () => {
   return (
     <header className={styles.Header}>
       <div className={styles.logo}>
         <Image src="/logo.png" height={32} width={32} />
-        <h1 className={styles.title}>Easy Log</h1>
+        <h1>
+          <Link href="/">
+            <a className={styles.title}>Easy Log</a>
+          </Link>
+        </h1>
       </div>
-      <button>로그인</button>
+      <Link href="/login">
+        <a className={styles.loginBtn}>로그인</a>
+      </Link>
     </header>
   );
 };
