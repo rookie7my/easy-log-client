654f7af9ddd62fe4b220904736aea9555c3784e8
jm kim <rookie2000g@gmail.com>
2022-02-24 15:22:25 +0900

    Set up msw

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,15 @@ import 'minireset.css/minireset.css';
 import '../styles/globals.css';
 import wrapper from '../store/store';
 
+if (process.env.NODE_ENV === 'development') {
+  if(typeof window !== 'undefined') {
+    const { worker } = require('../mocks/browser');
+    worker.start({
+      onUnhandledRequest: 'bypass'
+    });
+  }
+}
+
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
