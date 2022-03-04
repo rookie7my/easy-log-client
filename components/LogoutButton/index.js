693acb8f1febd4748109fc42772afc6062666173
jm kim <rookie2000g@gmail.com>
2022-03-04 14:46:52 +0900

    Remove(LogoutButton): Remove logout request related to redux

--- components/LogoutButton/index.js
+++ components/LogoutButton/index.js
@@ -1,26 +1,11 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { BsBoxArrowRight } from 'react-icons/bs';
-import { useDispatch } from 'react-redux';
 import styles from './styles.module.css';
-import { logout } from '../../reducers/currentUserSlice';
 
 const LogoutButton = () => {
-  const dispatch = useDispatch();
-  const [logoutRequestStatus, setLogoutRequestStatus] = useState('idle');
-
-  const onLogoutClicked = useCallback(async () => {
-    if(logoutRequestStatus === 'loading') {
-      return;
-    }
-    try {
-      setLogoutRequestStatus('loading');
-      await dispatch(logout()).unwrap();
+  const onLogoutClicked = useCallback( () => {
       console.log('logged out successfully');
-    } catch (err) {
-      setLogoutRequestStatus('idle');
-      console.error(err.message);
-    }
-  }, [logoutRequestStatus]);
+  }, []);
 
   return (
     <button onClick={onLogoutClicked} className={styles.LogoutButton}>
